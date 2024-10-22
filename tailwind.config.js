ccbfbf6cd30939c78b382c22ee49e018e64c3f2b
Morales Palomar <adrian.morales@cunef.edu>
2024-10-22 11:46:18 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      'NotoSangsJpExtraBold': ['NotoSangsJpExtraBold']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
